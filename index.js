0c2de5b4bb5b10b463a3e9e410f02a26f43d04f8
jesse keane <me@jessekeane.me>
2013-09-06 08:13:08 +0400

    skeleton

--- index.js
+++ index.js
@@ -8,11 +8,19 @@ var c = require('commander'),
     concat = require('concat-stream'),
     isCli = (require.main === module);
 
+function Geiger(options) {
+  if (!(this instanceof Geiger)) return new Geiger(options);
+  this.settings = options || {};
+
+  return this;
+}
+
 if (isCli) {
   c
     .version('0.0.0')
-    .usage('[options] [srcdir] dstdir')
-    .option('-w, --watch', 'watch srcdir for changes and compile automatically')
+    .usage('[options] outputdir')
+    .option('d, --dir <dir>', 'use <dir> instead of cwd')
+    .option('-w, --watch', 'watch for changes and compile automatically')
     .parse(process.argv);
 
   if (!c.args.length) c.help();
