fce11c7f16859be6b861ffbf72cf2ec11f37c19b
jesse keane <me@jessekeane.me>
2013-09-06 08:28:29 +0400

    parse options, look for config file

--- index.js
+++ index.js
@@ -4,13 +4,21 @@ var c = require('commander'),
     md = require('marked'),
     ejs = require('ejs'),
     fs = require('fs'),
+    path = require('path'),
     through = require('through'),
     concat = require('concat-stream'),
     isCli = (require.main === module);
 
 function Geiger(options) {
   if (!(this instanceof Geiger)) return new Geiger(options);
-  this.settings = options || {};
+  options = options || {};
+  var activeDir = options.dir ? path.normalize(options.dir) : process.cwd(),
+      config = null;
+  if (fs.existsSync(activeDir + '/.geiger.json')) config = require(activeDir + '/.geiger.json');
+  this.settings = {
+    dir: activeDir,
+    config: config
+  };
 
   return this;
 }
@@ -18,7 +26,7 @@ function Geiger(options) {
 if (isCli) {
   c
     .version('0.0.0')
-    .usage('[options] outputdir')
+    .usage('[options]')
     .option('d, --dir <dir>', 'use <dir> instead of cwd')
     .option('-w, --watch', 'watch for changes and compile automatically')
     .parse(process.argv);
